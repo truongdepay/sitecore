950898e2df7379e9bdaccb9250c4df2254ea7436
unknown <TruongNV>
2019-01-23 18:06:54 +0700

    update gmo

--- application/modules/managerCat/controllers/Index.php
+++ application/modules/managerCat/controllers/Index.php
@@ -21,6 +21,9 @@ class Index extends MX_Controller
         $this->load->library([
             'session'
         ]);
+        $this->load->config('config_notification');
+        $this->noError = config_item('notifyError');
+        $this->load->model('catModel');
     }
 
     public function index()
@@ -48,12 +51,69 @@ class Index extends MX_Controller
         }
     }
 
+    private function manager()
+    {
+        $data = [];
+        $data['siteTitle'] = 'Quản lý danh mục';
+
+        $result = $this->catModel->getResult();
+        $data['result'] = $result;
+
+        $template = 'manager';
+        $this->loadView($template, $data);
+    }
+
     private function create()
     {
         $data = [];
         $data['siteTitle'] = 'Thêm mới danh mục';
 
+        if ($this->input->server('REQUEST_METHOD') == 'POST') {
+            $error = [];
+            $title = $this->input->post('title');
+            $slugs = $this->input->post('slugs');
+            $parent = $this->input->post('parent');
+            $status = $this->input->post('status');
+            $type = $this->input->post('type');
 
+            $flashData = [
+                'slugs' => $slugs,
+                'status' => $status,
+                'parent' => $parent,
+                'title' => $title,
+                'type' => $type
+            ];
+            $this->session->set_flashdata('dataCat', $flashData);
+
+            if (!titleCheck($title)) {
+                $error['title'] = $this->noError['title'];
+            }
+
+            if (!slugsCheck($slugs)) {
+                $error['slugs'] = $this->noError['slugs'];
+            }
+
+            $checkExistSlugs = $this->catModel->checkExist('slugs', $slugs);
+            if ($checkExistSlugs != 0) {
+                $error['dupSlugs'] = $this->noError['dupSlugs'];
+            }
+
+            if (empty($error)) {
+                $catData = [
+                    'title' => $title,
+                    'slugs' => $slugs,
+                    'status' => $status,
+                    'parent' => $parent,
+                    'type' => $type,
+                    'date_create' => time()
+                ];
+                $this->catModel->add($catData);
+                $this->session->set_flashdata('success', true);
+                redirect('managerCat/index/index?action=manager');
+            } else {
+                $this->session->set_flashdata('error', $error);
+            }
+        }
 
         $template = 'create';
         $this->loadView($template, $data);
@@ -67,38 +127,38 @@ class Index extends MX_Controller
         $this->load->view(config_item('pathFooter'), $data);
     }
 
-//    public function createSlug()
-//    {
-//        $this->load->library([
-//            'slugs',
-//            'output'
-//        ]);
-//        $action = $this->input->get('action');
-//        $id = $this->input->get('id');
-//
-//        if ($this->input->server('REQUEST_METHOD') == 'POST') {
-//            $title = $this->input->post('title');
-//            $slugs = $this->slugs->create($title);
-//
-//            if ($action == 'edit') {
-//                $check = $this->productModel->checkExist('slugs', $slugs, $id);
-//            } else {
-//                $check = $this->productModel->checkExist('slugs', $slugs);
-//            }
-//
-//            if ($check != 0) {
-//                $notify = $this->noError['dupSlugs'];
-//            } else {
-//                $notify = '';
-//            }
-//
-//            $this->output
-//                ->set_content_type('application/json')
-//                ->set_output(json_encode([
-//                    'slugs' => $slugs,
-//                    'notify' => $notify
-//                ]));
-//        }
-//    }
+    public function createSlug()
+    {
+        $this->load->library([
+            'slugs',
+            'output'
+        ]);
+        $action = $this->input->get('action');
+        $id = $this->input->get('id');
+
+        if ($this->input->server('REQUEST_METHOD') == 'POST') {
+            $title = $this->input->post('title');
+            $slugs = $this->slugs->create($title);
+
+            if ($action == 'edit') {
+                $check = $this->catModel->checkExist('slugs', $slugs, $id);
+            } else {
+                $check = $this->catModel->checkExist('slugs', $slugs);
+            }
+
+            if ($check != 0) {
+                $notify = $this->noError['dupSlugs'];
+            } else {
+                $notify = '';
+            }
+
+            $this->output
+                ->set_content_type('application/json')
+                ->set_output(json_encode([
+                    'slugs' => $slugs,
+                    'notify' => $notify
+                ]));
+        }
+    }
 
 }
