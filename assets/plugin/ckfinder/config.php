ab0ddd8e35f63080224fdf420ec232e6b92c02d3
Sama Zenn <ngoctruong.humg2012@gmail.com>
2019-02-21 23:59:01 +0700

    uddate

--- assets/plugin/ckfinder/config.php
+++ assets/plugin/ckfinder/config.php
@@ -1,52 +1,5 @@
 <?php
-class Session {
-    public static function unserialize($session_data) {
-        $method = ini_get("session.serialize_handler");
-        switch ($method) {
-            case "php":
-                return self::unserialize_php($session_data);
-                break;
-            case "php_binary":
-                return self::unserialize_phpbinary($session_data);
-                break;
-            default:
-                throw new Exception("Unsupported session.serialize_handler: " . $method . ". Supported: php, php_binary");
-        }
-    }
 
-    private static function unserialize_php($session_data) {
-        $return_data = array();
-        $offset = 0;
-        while ($offset < strlen($session_data)) {
-            if (!strstr(substr($session_data, $offset), "|")) {
-                throw new Exception("invalid data, remaining: " . substr($session_data, $offset));
-            }
-            $pos = strpos($session_data, "|", $offset);
-            $num = $pos - $offset;
-            $varname = substr($session_data, $offset, $num);
-            $offset += $num + 1;
-            $data = unserialize(substr($session_data, $offset));
-            $return_data[$varname] = $data;
-            $offset += strlen(serialize($data));
-        }
-        return $return_data;
-    }
-
-    private static function unserialize_phpbinary($session_data) {
-        $return_data = array();
-        $offset = 0;
-        while ($offset < strlen($session_data)) {
-            $num = ord($session_data[$offset]);
-            $offset += 1;
-            $varname = substr($session_data, $offset, $num);
-            $offset += $num;
-            $data = unserialize(substr($session_data, $offset));
-            $return_data[$varname] = $data;
-            $offset += strlen(serialize($data));
-        }
-        return $return_data;
-    }
-}
 /*
  * CKFinder Configuration File
  *
@@ -66,19 +19,7 @@ ini_set('display_errors', 1);
 
 
 // get the session_id from codeigniter cookie.
-$session_id = $_COOKIE['ci_session'];
-// get the session data by session_id,replace to your session save path
 
-$content = file_get_contents('../../../application/common/session/ci_session' . $session_id);
-session_start();
-$session = Session::unserialize($content);
-
-if (isset($session['login']) && $session['login']['check'] === true ) {
-    $_SESSION['check'] = true;
-} else {
-    $_SESSION['check'] = false;
-}
-var_dump($_SESSION['check']);
 // Start new or resume existing session
 /*============================ General Settings =======================================*/
 // https://ckeditor.com/docs/ckfinder/ckfinder3-php/configuration.html
@@ -89,7 +30,7 @@ $config = array();
 // https://ckeditor.com/docs/ckfinder/ckfinder3-php/configuration.html#configuration_options_authentication
 
 $config['authentication'] = function () {
-    if ($_SESSION['check']) {
+    if (isset($_SESSION['check'])) {
         return true;
     } else {
         return false;
